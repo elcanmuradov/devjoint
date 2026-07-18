b3383cc7c5b00944ecf7aa952bf3a3b475a67c14
elcanmuradov <elcanmuradov122@gmail.com>
2026-07-18 11:08:56 +0400

    OpenAPI / Swagger implementation

--- week1/Library Management/src/main/java/com/devjoint/librarymanagement/controller/AuthorController.java
+++ week1/Library Management/src/main/java/com/devjoint/librarymanagement/controller/AuthorController.java
@@ -6,6 +6,9 @@ import com.devjoint.librarymanagement.dto.author.AuthorRequest;
 import com.devjoint.librarymanagement.dto.book.BookDto;
 import com.devjoint.librarymanagement.service.AuthorService;
 import com.devjoint.librarymanagement.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -24,31 +27,37 @@ public class AuthorController {
     private final AuthorService authorService;
     private final BookService bookService;
 
+    @Operation(summary = "Create author", description = "Create author via name, birthdate, and  it's own description")
     @PostMapping("/create")
     public ResponseEntity<ApiResponse<AuthorDto>> addAuthor(@RequestBody @Valid AuthorRequest request){
         return ResponseEntity.status(201).body(ApiResponse.success(authorService.createAuthor(request)));
     }
 
+    @Operation(summary = "Update user by id")
     @PutMapping("/{id}/update")
-    public ResponseEntity<ApiResponse<AuthorDto>> updateAuthor(@PathVariable UUID id, @RequestBody AuthorRequest request){
+    public ResponseEntity<ApiResponse<AuthorDto>> updateAuthor(@Parameter(description = "User's id must be UUID", examples = @ExampleObject(value = "88cd0def-561c-481c-94c5-ca432d6dcaa")) @PathVariable UUID id, @RequestBody AuthorRequest request){
         return ResponseEntity.status(200).body(ApiResponse.success(authorService.updateAuthor(id, request)));
     }
 
+    @Operation(summary ="Delete author via id")
     @DeleteMapping("/{id}/delete")
-    public ResponseEntity<ApiResponse<Void>> deleteAuthor(@PathVariable UUID id){
+    public ResponseEntity<ApiResponse<Void>> deleteAuthor(@Parameter(description = "User's id must be UUID", examples = @ExampleObject(value = "88cd0def-561c-481c-94c5-ca432d6dcaa")) @PathVariable UUID id){
        return ResponseEntity.status(200).body(ApiResponse.success(authorService.deleteAuthor(id)));
     }
 
     @GetMapping("")
+    @Operation(summary = "Get all authors")
     public ResponseEntity<ApiResponse<List<AuthorDto>>> getAllAuthors(){
         return  ResponseEntity.status(200).body(ApiResponse.success(authorService.getAllAuthors()));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get author via id")
     public ResponseEntity<ApiResponse<AuthorDto>> getAuthorById(@PathVariable UUID id){
         return ResponseEntity.status(200).body(ApiResponse.success(authorService.getAuthor(id)));
     }
 
+    @Operation(summary = "Get author's books")
     @GetMapping("/{id}/books")
     public ResponseEntity<ApiResponse<Page<BookDto>>> getBooksByAuthorId(@PathVariable UUID id, Integer page, Integer pageSize){
         return ResponseEntity.status(200).body(ApiResponse.success(bookService.getBooksByAuthorId(id,page,pageSize)));
--- week1/Library Management/src/main/java/com/devjoint/librarymanagement/controller/BookController.java
+++ week1/Library Management/src/main/java/com/devjoint/librarymanagement/controller/BookController.java
@@ -5,6 +5,7 @@ import com.devjoint.librarymanagement.dto.book.BookDto;
 import com.devjoint.librarymanagement.dto.book.BookRequest;
 import com.devjoint.librarymanagement.entity.Book;
 import com.devjoint.librarymanagement.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,26 +23,31 @@ import java.util.UUID;
 public class BookController {
     private final BookService bookService;
 
+    @Operation(summary = "Create book", description = "Create book via authorId, title , and genre")
     @PostMapping("/create")
     public ResponseEntity<ApiResponse<BookDto>> addBook(@RequestBody @Valid BookRequest request){
         return ResponseEntity.status(201).body(ApiResponse.success(bookService.createBook(request)));
     }
 
+    @Operation(summary = "Update book", description = "Update book via authorId, title , and genre")
     @PutMapping("/{id}/update")
     public ResponseEntity<ApiResponse<BookDto>> updateBook(@PathVariable UUID id, @RequestBody BookRequest request){
         return ResponseEntity.status(200).body(ApiResponse.success(bookService.updateBook(id, request)));
     }
 
+    @Operation(summary = "Delete book via id ")
     @DeleteMapping("/{id}/delete")
     public ResponseEntity<ApiResponse<Void>> deleteBook(@PathVariable UUID id){
         return ResponseEntity.status(200).body(ApiResponse.success(bookService.deleteBook(id)));
     }
 
+    @Operation(summary = "Get all books")
     @GetMapping()
     public ResponseEntity<ApiResponse<Page<BookDto>>> getAllBooks(Integer page, Integer pageSize){
         return ResponseEntity.status(200).body(ApiResponse.success(bookService.getAllBooks(page,pageSize)));
     }
 
+    @Operation(summary = "Get book via id ")
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<BookDto>> getBookById(@PathVariable UUID id){
         return ResponseEntity.status(200).body(ApiResponse.success(bookService.getBookById(id)));

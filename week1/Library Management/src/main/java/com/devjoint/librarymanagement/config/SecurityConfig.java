7e55d9327bfcbc6725298646821f15ed5d166143
elcanmuradov <elcanmuradov122@gmail.com>
2026-07-25 14:58:20 +0400

    Security Filter Chain configuration

--- week1/Library Management/src/main/java/com/devjoint/librarymanagement/config/SecurityConfig.java
+++ week1/Library Management/src/main/java/com/devjoint/librarymanagement/config/SecurityConfig.java
@@ -2,15 +2,35 @@ package com.devjoint.librarymanagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/auth/login","/api/auth/register").permitAll()
+                        .anyRequest().authenticated()
+
+                )
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/"));
+        return http.build();
+    }
+
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 
 }
